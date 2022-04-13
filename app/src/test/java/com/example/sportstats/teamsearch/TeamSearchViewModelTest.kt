6d44af1a3f0a3a9c545a7587b441f85d134cc983
Rody Lorenzo <rodylorenzowork@gmail.com>
2022-04-13 16:12:05 -0400

    Test TeamSearchViewModel

--- app/src/test/java/com/example/sportstats/teamsearch/TeamSearchViewModelTest.kt
+++ app/src/test/java/com/example/sportstats/teamsearch/TeamSearchViewModelTest.kt
@@ -1,14 +1,58 @@
 package com.example.sportstats.teamsearch
 
-import org.junit.Assert.*
-
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.sportstats.ServiceLocator
+import com.example.sportstats.network.BaseTest
+import com.example.sportstats.network.FakeTeamService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.nullValue
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
-class TeamSearchViewModelTest {
+@ExperimentalCoroutinesApi
+class TeamSearchViewModelTest : BaseTest() {
 
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
 
+    // Subject under test
+    private lateinit var teamSearchViewModel: TeamSearchViewModel
+
+    private lateinit var serviceTeam: FakeTeamService
+
+    @Before
+    fun setup() {
+        Dispatchers.setMain(StandardTestDispatcher())
+        serviceTeam = FakeTeamService()
+        ServiceLocator.teamService = serviceTeam
+        teamSearchViewModel = TeamSearchViewModel(serviceTeam)
+    }
+
+    @After
+    fun cleanup() {
+        ServiceLocator.resetService()
+    }
 
     @Test
-    fun searchTeamByName_noName() {
+    fun searchTeamByName_loading() = runTest {
+        // When
+        val job = launch {
+            teamSearchViewModel.searchTeamByName()
+        }
+        // Then
+        assertThat(teamSearchViewModel.status.value, `is`(nullValue()))
+        // When
+        job.join()
+        // Then
+        assertThat(teamSearchViewModel.status.value, `is`(ConnectionApiStatus.DONE))
     }
 }
